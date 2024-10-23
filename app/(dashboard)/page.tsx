8d1d91e80601b64f28724f575e634851535ccf94
Mirko Saugo <himynameismirko@gmail.com>
2024-10-23 19:34:36 +0200

    updates

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+
 // import { useGetAccounts } from "@/features/accounts/api/useGetAccounts";
 
 export default function Home() {
-  return <div>MAIN PAGE</div>;
+  const handleShowToast = () => {
+    toast.success("Hello World");
+  };
+
+  return (
+    <div>
+      MAIN PAGE
+      <br />
+      <br />
+      <Button onClick={handleShowToast}>Show Toast</Button>
+    </div>
+  );
 }
