79d81d50a7c54554c764b6b303e479b420eebbb9
Mirko Saugo <himynameismirko@gmail.com>
2025-01-29 18:21:16 +0100

    Refactor middleware to clean up commented code and improve header handling

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,4 @@
-// CLERK
-// The clerkMiddleware helper enables authentication and is where you'll configure your protected routes.
-// import { createRouteMatcher } from "@clerk/nextjs/server";
-
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
 // const isProtectedRoute = createRouteMatcher([
 //   '/',
@@ -10,22 +6,14 @@ import { NextRequest, NextResponse } from "next/server"
 //   '/forum(.*)', : all forum routes are protected
 // ]);
 
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-//   return NextResponse.next();
-
-// });
-
-
 // export const config = {
 //   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 // };
 
 export function middleware(request: NextRequest) {
   // Clone the request headers and set a new header `x-hello-from-middleware1`
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-config-from-middleware', '-')
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-config-from-middleware", "-");
 
   // You can also set request headers in NextResponse.next
   const response = NextResponse.next({
@@ -33,9 +21,9 @@ export function middleware(request: NextRequest) {
       // New request headers
       headers: requestHeaders,
     },
-  })
+  });
 
   // Set a new response header `x-hello-from-middleware2`
-  response.headers.set('x-config-2-from-middleware', '.')
-  return response
+  response.headers.set("x-config-2-from-middleware", ".");
+  return response;
 }
