39f7c00545c18d0d2f353bccf4d62cd5f8728015
Mirko Saugo <himynameismirko@gmail.com>
2025-02-01 14:18:38 +0100

    Add twin selection dropdown to edit twin details on Twins page

--- app/(home)/twins/page.tsx
+++ app/(home)/twins/page.tsx
@@ -6,17 +6,38 @@ import Link from "next/link";
 
 import TwinsList from "@/features/twins/components/TwinsList";
 import TwinEdit from "@/features/twins/components/TwinEdit";
+import { useGetTwins } from "@/features/twins/api/useGetTwins";
+import { use, useMemo, useState } from "react";
 
 export default function TwinsPage() {
+  const { data = [], isLoading, isError } = useGetTwins();
+  const [currentTwin, setCurrentTwin] = useState<null | string>(null);
+
+  const twinsIds = useMemo(() => data.map((twin) => twin.id), [data.length]);
+
   return (
     <div className="pt-10">
       <h1 className="text-6xl font-bold">Twins Page</h1>
-      <TwinEdit id="1" />
+      <br />
+      <div>
+        <select onChange={(e) => setCurrentTwin(e.target.value)}>
+          <option>Select a twin to edit</option>
+          {twinsIds.map((id) => (
+            <option key={id} value={id}>
+              {id}
+            </option>
+          ))}
+        </select>
+      </div>
+      {currentTwin && <TwinEdit id={currentTwin} />}
       <br />
       <br />
       <hr />
       <br />
       <TwinsList />
+      <br />
+      <hr />
+      <br />
       <Link className={buttonVariants({ variant: "outline" })} href="/">
         Back to Home
       </Link>
