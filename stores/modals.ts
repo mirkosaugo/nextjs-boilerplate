9e06962202449e20794a36360b7d34ceb3ae4d2e
Mirko Saugo <himynameismirko@gmail.com>
2025-02-04 17:10:16 +0100

    Refactor modals store by removing unused properties and methods

--- stores/modals.ts
+++ stores/modals.ts
@@ -3,23 +3,10 @@ import { createWithEqualityFn } from "zustand/traditional";
 
 import { createStoreWithSelectors } from "@lib/utils";
 
-type ModalsStore = {
-  IdInEdit?: string;
-  isEditAccountModalOpen: boolean;
-
-  setEditAccountModalOpen: (id: string) => void;
-  setEditAccountModalClose: () => void;
-};
-
+type ModalsStore = {};
 
 const modalsStore = createWithEqualityFn<ModalsStore>()(
-  devtools((set) => ({
-    IdInEdit: undefined,
-    isEditAccountModalOpen: false,
-
-    setEditAccountModalOpen: (id) => set((state) => ({ ...state, isEditAccountModalOpen: true, IdInEdit: id })),
-    setEditAccountModalClose: () => set((state) => ({ ...state, isEditAccountModalOpen: false, IdInEdit: undefined })),
-  })),
+  devtools((set) => ({})),
   Object.is,
 );
 
